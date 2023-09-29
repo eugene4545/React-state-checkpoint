4e34901f88621daa25bb57544c06fb6ee98d26b9
eugene4545 <eugeneonuoha72@gmail.com>
2023-09-29 13:57:50 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: {
+        fullName: 'Ethan Harris',
+        bio: 'A being passionate about data',
+        imgSrc: '/photo-1583864697784-a0efc8379f70.jpg',
+        profession: 'Machine learning Engineer',
+      },
+      show: true,
+      timeInterval: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    // Capture the moment when the component was mounted
+    this.setState({
+      timeInterval: new Date(),
+    });
+  }
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeInterval } = this.state;
+
+    return (
+      <div className='app'>
+        <h1>My Profile</h1>
+        <button onClick={this.toggleProfile}>
+          {show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {show && (
+          <div className='card'>
+            <h2>{fullName}</h2>
+            <p>Bio: {bio}</p>
+            <p>Profession: {profession}</p>
+            <img src={imgSrc} alt={fullName} />
+          </div>
+        )}
+
+        <p>Time since mount: {timeInterval.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
